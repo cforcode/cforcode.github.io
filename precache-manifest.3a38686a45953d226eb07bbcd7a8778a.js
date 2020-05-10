8f4f00d12f589a9bf0d2cc61466055c961283c5f
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2020-05-11 00:12:03 +0530

    Add files via upload

---
+++ precache-manifest.3a38686a45953d226eb07bbcd7a8778a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f5c16e57cdc944bb3885a0a973ef99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a0dd58bd9dcce968071",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "4f52471bd327453eb2b1",
+    "url": "/static/css/main.af426e46.chunk.css"
+  },
+  {
+    "revision": "5a0dd58bd9dcce968071",
+    "url": "/static/js/2.bf65eb65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bf65eb65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f52471bd327453eb2b1",
+    "url": "/static/js/main.921ec1c7.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "870bca85b11a22c15e9eea850904d6b2",
+    "url": "/static/media/login.870bca85.png"
+  }
+]);
