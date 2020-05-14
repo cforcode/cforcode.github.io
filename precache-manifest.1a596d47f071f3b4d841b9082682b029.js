dc4da4b0ddfd23374780380019d4fb896657d32f
Manas Sahoo <manas_sahoo@outlook.com>
2020-05-14 12:41:55 +0530

    code add

---
+++ precache-manifest.1a596d47f071f3b4d841b9082682b029.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439539be6f9d62dc961fdd928e781db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a0dd58bd9dcce968071",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "fe28eeab79327563b1ac",
+    "url": "/static/css/main.af426e46.chunk.css"
+  },
+  {
+    "revision": "5a0dd58bd9dcce968071",
+    "url": "/static/js/2.bf65eb65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bf65eb65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe28eeab79327563b1ac",
+    "url": "/static/js/main.823f94d9.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "870bca85b11a22c15e9eea850904d6b2",
+    "url": "/static/media/login.870bca85.png"
+  }
+]);
