5fa2dd806be5dab63d3b9a1486cc276784b282a5
Manas Sahoo <manas_sahoo@outlook.com>
2020-08-07 01:35:26 +0530

    seo

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45ff7828d3fa080bd0ed26612cda1643.js"
+  "/precache-manifest.9a3db4f3633e8c63a9d603a31f9e2932.js"
 );
 
 self.addEventListener('message', (event) => {
